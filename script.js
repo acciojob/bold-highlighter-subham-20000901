817de67b8b08e2c9e7a61e00c86ac3ab8d98338c
subham-20000901 <sinhasubh2000@gmail.com>
2026-02-16 12:57:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function highlight() {
     //Write your code here
 let strongs = document.querySelectorAll("strong");
-   strongs.forEach((strong) {
+   strongs.forEach((strong) => {
      strong.style.color = "rgb(0, 128, 0)";	
 })
 
@@ -11,7 +11,7 @@ let strongs = document.querySelectorAll("strong");
 function return_normal() {
     //Write your code here
 let strongs = document.querySelectorAll("strong");
-    strongs.forEach((strong) {
+    strongs.forEach((strong) => {
       strong.style.color = "rgb(0, 0, 0)";	
   })
 	
