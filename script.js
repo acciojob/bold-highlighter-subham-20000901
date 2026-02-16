5cdb79c0f7326bdd597960afe8cedbb9de4a10d1
subham-20000901 <sinhasubh2000@gmail.com>
2026-02-16 12:51:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 function highlight() {
     //Write your code here
+let strongs = document.querySelectorAll("strong");
+   strongs.forEach((strong) {
+     strong.style.color = "rgb(0, 128, 0)";	
+})
 
-
-}
+};
 
 
 function return_normal() {
     //Write your code here
-
-    
-}
+let strongs = document.querySelectorAll("strong");
+    strongs.forEach((strong) {
+      strong.style.color = "rgb(0, 0, 0)";	
+  })
+	
+};
